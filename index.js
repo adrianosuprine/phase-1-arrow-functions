7c4a0c8c07e3e227cd144f17e4ecf67f24420cbc
adrianosuprine <adrianosuprine@gmail.com>
2023-12-14 08:15:33 +0300

    Use arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(){
+    return 2000/100 
+}
+// console.log(divide());
+let square=parameter1=>parameter1**2 ;
+console.log(square(4));
+const add=(parameter1,parameter2) => {
+    return parameter1 + parameter2 ;
+}
+console.log(add(2,3))
